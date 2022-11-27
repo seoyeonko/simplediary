3a86c8838f41150b712970c8cf8cf5cf52b63b7e
seoyeonko <seoyeon99o@naver.com>
2022-11-27 22:35:34 +0900

    #42. 6-7. React Lifecycle 제어하기 - useEffect (2 - mount, unmount)

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,54 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
-const LifeCycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
-
-  // useEffect(callback[, dependency_array])
-  // callback: func
-  // dependency_array (의존성 배열): 해당 배열 값이 변하면 callback 함수 실행
-  //  - []: 빈배열 -> Mount 시에만 동작
-  //  - 생략 -> Mount & Update 시 동작
-  //  - [data]: 해당 데이터 update시 동작
+// 자식 컴포넌트
+// 반드시 한 파일에 한 컴포넌트만 존재해야하는 것은 아님.
+const UnmountTest = () => {
+  // 특정값의 변경 여부 추적 가능
   useEffect(() => {
+    // Mount 시점에 실행
     console.log('Mount!');
+
+    // Unmount 시점에 실행
+    return () => {
+      console.log('Unmount!');
+    };
   }, []);
 
-  useEffect(() => {
-    console.log('Update!');
-  });
+  return <div>Unmount Testing Component</div>;
+};
 
-  useEffect(() => {
-    console.log(`count is update: ${count}`);
-    if (count > 5) {
-      alert('countr가 5를 넘었습니다. 따라서 1로 초기화합니다.');
-      setCount(1);
-    }
-  }, [count]);
-
-  useEffect(() => {
-    console.log(`text is update: ${text}`);
-  }, [text]);
+const LifeCycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: '20px' }}>
-      <div>
-        {count}
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {/* 
+        단락회로 평가 
+        - isVisible = true; UnmountTest 렌더링
+        - isVisible = false; 단락회로 평가 발생 ->  UnmountTest 렌더링 X
+      */}
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
