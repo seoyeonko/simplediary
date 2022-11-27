b62355a55b89a23a90b48d1b42462cd033172f77
seoyeonko <seoyeon99o@naver.com>
2022-11-27 23:29:40 +0900

    #45. 6-10. 최적화 1 - useMemo (최적화 1 - 연산 결과 재사용)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { getAllByPlaceholderText } from '@testing-library/react';
-import { useEffect, useRef, useState } from 'react';
+import { useMemo, useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -31,7 +30,7 @@ import DiaryList from './DiaryList';
 
 // https://jsonplaceholder.typicode.com/comments
 
-function App() {
+const App = () => {
   // DirayEditor, DiaryList 컴포넌트가 함께 사용할 일기 데이터
   const [data, setData] = useState([]); // 빈 배열: 일기 0개
 
@@ -93,13 +92,37 @@ function App() {
     );
   };
 
+  // useMemo: 연산을 최적화 하고 싶은 함수를 감싸주면 됨!
+  // useMemo(callback, dependency_array)
+  // - callback: 콜백함수의 return 값을 최적화함
+  // - dependency_array: 해당 배열 값이 변경될 때만 callback 수행
+  // **주의! useMemo는 첫번째 인자 callback함수의 값을 리턴함. 즉 값을 리턴함으로 함수가 아님!!
+  const getDiaryAnalysis = useMemo(() => {
+    // 2번 찍힘 -> why? 맨 처음 데이터 불러오기 전에 (즉, 빈배열일 때) 1회, 데이터 불러오고 난 후 2회
+    // 새로운 일기가 추가될 때마다 콘솔에 찍힘
+    // 하지만! [수정] 작업에서도 콘솔에 찍힘 -> 감정은 수정 불가능하기 때문에 불필요한 연산 발생
+    console.log('일기 분석 시작');
+    const goodCount = data.filter((it) => it.emotion > 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  // const { goodCount, badCount, goodRatio } = getDiaryAnalysis();
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis; // **주의! useMemo로 감싸져 있으니 함수가 아닌 값으로 사용해야 함!!
+
   return (
     <div className="App">
       {/* <LifeCycle /> */}
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기: {data.length}</div>
+      <div>기분 좋은 일기 개수: {goodCount}</div>
+      <div>기분 나쁜 일기 개수: {badCount}</div>
+      <div>기분 좋은 일기 비율: {goodRatio}%</div>
       <DiaryList diaryList={data} onEdit={onEdit} onRemove={onRemove} />
     </div>
   );
-}
+};
 
 export default App;
